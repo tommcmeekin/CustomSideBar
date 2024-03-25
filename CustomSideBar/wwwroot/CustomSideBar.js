f383a11216a7faf48e35f8788474d9172dce908b
Tom McMeekin <tom.mcmeekin123@outlook.com>
2024-03-25 20:47:52 +0000

    Add project files.

---
+++ CustomSideBar/wwwroot/CustomSideBar.js
@@ -0,0 +1,9 @@
+﻿function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('show');
+    if (sidebar.classList.contains('show')) {
+        document.querySelector('.toggle-btn').innerHTML = '✕';
+    } else {
+        document.querySelector('.toggle-btn').innerHTML = '☰';
+    }
+}
