697a3edc4bf0cbe7e27df8ede9ffe3759e8d1c66
Tom McMeekin <tom.mcmeekin123@outlook.com>
2024-03-26 22:47:36 +0000

    fixed-mobile-menu

--- CustomSideBar/wwwroot/CustomSideBar.js
+++ CustomSideBar/wwwroot/CustomSideBar.js
@@ -1,5 +1,5 @@
 ﻿function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
+    const sidebar = document.querySelector('.sidemenu');
     sidebar.classList.toggle('show');
     if (sidebar.classList.contains('show')) {
         document.querySelector('.toggle-btn').innerHTML = '✕';
